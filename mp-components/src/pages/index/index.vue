8c3714a7c5de02801c7569fcdcfe7e971d26f72f
JingkaiZhao <jingkai.zhao@foxmail.com>
2018-05-16 17:27:49 +0800

    initlize demo

---
+++ mp-components/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <card :text="motto"></card>
+    <div>date: {{ date }}</div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  mpType: 'component',
+  properties: {
+    date: String
+  },
+
+  data () {
+    return {
+      motto: 'Hello World from Native Component'
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
